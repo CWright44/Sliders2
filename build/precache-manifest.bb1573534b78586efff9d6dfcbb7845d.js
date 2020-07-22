d46a1df685175da0d90a72efbac5e6adac2a5043
CWright44 <43626065+CWright44@users.noreply.github.com>
2020-07-22 10:11:44 -0400

    adding page dependecies

---
+++ build/precache-manifest.bb1573534b78586efff9d6dfcbb7845d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e536295c108e38cb55bc",
+    "url": "/static/css/main.5512a13c.chunk.css"
+  },
+  {
+    "revision": "e536295c108e38cb55bc",
+    "url": "/static/js/main.e536295c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "fe6e663e0fa2b2d486d1",
+    "url": "/static/js/2.fe6e663e.chunk.js"
+  },
+  {
+    "revision": "4e4c4c0ef8126bb19367c0b8a0e5fc7c",
+    "url": "/index.html"
+  }
+];
